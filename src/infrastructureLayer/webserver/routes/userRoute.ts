533083d92551fb9f17d6fe501f50254e22f6423b
adwxithc <adwaithjanardhanan0@gmail.com>
2024-04-05 18:59:25 +0530

    implemented: request body validation using express validator

--- src/infrastructureLayer/webserver/routes/userRoute.ts
+++ src/infrastructureLayer/webserver/routes/userRoute.ts
@@ -1,4 +1,3 @@
-
 import { body } from 'express-validator';
 import { Router } from 'express';
 
@@ -7,53 +6,83 @@ import { validateRequest } from '../middlewares';
 import { userController } from './injections/injection';
 import { Req, Res } from '../../types/expressTypes';
 
-
 export function userRoute(router: Router) {
     router.post(
-        '/signup',[
-            body('firstName').isLength({min:3}).withMessage('Name must be atleast 3 characters long'),
-            body('userName').isLength({min:3}).withMessage('User name must be atleast 3 character long'),
+        '/signup',
+        [
+            body('firstName')
+                .isLength({ min: 3 })
+                .withMessage('Name must be atleast 3 characters long'),
+            body('userName')
+                .isLength({ min: 3 })
+                .withMessage('User name must be atleast 3 character long'),
             body('email').isEmail().withMessage('Email must be valid'),
-            body('password').isLength({min:4, max: 20})
-                .withMessage('password must be between 4 and 20 characters')
+            body('password')
+                .trim()
+                .isLength({ min: 4, max: 20 })
+                .withMessage('password must be between 4 and 20 characters'),
         ],
         validateRequest,
-        async(req: Req, res: Res) => {
-            await userController.registerUser(req, res); 
-        });
+        async (req: Req, res: Res) => {
+            await userController.registerUser(req, res);
+        }
+    );
 
     router.post(
         '/verify-otp',
-        async(req:Req, res:Res)=>{
+        [body('otp').isLength({ min: 6, max: 6 }).withMessage('invalid otp')],
+        validateRequest,
+        async (req: Req, res: Res) => {
             await userController.createUser(req, res);
-        });
+        }
+    );
 
     router.post(
         '/signin',
-        async (req:Req, res:Res)=>{
-            await userController.signin(req,res);
-        });
+        [
+            body('email').isEmail().withMessage('Email must be valid'),
+            body('password')
+                .trim()
+                .isLength({ min: 4, max: 20 })
+                .withMessage('password must be between 4 and 20 characters'),
+        ],
+        validateRequest,
+        async (req: Req, res: Res) => {
+            await userController.signin(req, res);
+        }
+    );
 
     router.post(
         '/forgot-password',
-        async(req:Req, res:Res)=>{
-            await userController.sendPasswordResetMail(req,res);
-        });
+        [body('email').isEmail().withMessage('Email must be valid')],
+        validateRequest,
+        async (req: Req, res: Res) => {
+            await userController.sendPasswordResetMail(req, res);
+        }
+    );
 
     router.post(
         '/verify-password-reset',
-        async(req:Req, res:Res)=>{
-            await userController.verifyPasswordReset(req,res);
+        [body('otp').isLength({ min: 6, max: 6 }).withMessage('invalid otp')],
+        validateRequest,
+        async (req: Req, res: Res) => {
+            await userController.verifyPasswordReset(req, res);
         }
     );
 
     router.post(
         '/reset-password',
-        async(req:Req, res:Res) =>{
+        [
+            body('password')
+                .trim()
+                .isLength({ min: 4, max: 20 })
+                .withMessage('password must be between 4 and 20 characters'),
+        ],
+        validateRequest,
+        async (req: Req, res: Res) => {
             await userController.resetPassword(req, res);
-        });
+        }
+    );
 
     return router;
 }
-
-
